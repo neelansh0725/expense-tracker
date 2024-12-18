d529a75fe06c95839ca353446e949a1dcaac5658
ishant174 <ishantg174@gmail.com>
2024-12-18 21:52:20 +0530

    Added currency setting

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -4,19 +4,22 @@ import { Expense } from "../Expense/Expense";
 import { Income } from "../Income/Income";
 import { Transaction } from "../Transaction/Transaction";
 import { Setting } from "../Setting/Setting";
-
+import cookie from "react-cookies";
 export const Menu = (props) => {
   const [expense, setExpense] = useState([]);
+  const currentCurrency = cookie.load("expense_currency");
 
   const handleMenu = (menu) => {
     if (menu == "dashboard") {
-      props.menuselected(<Dashboard />);
+      props.menuselected(<Dashboard currency={currentCurrency} />);
     } else if (menu == "transaction") {
       props.menuselected(<Transaction />);
     } else if (menu == "income") {
-      props.menuselected(<Income />);
+      props.menuselected(<Income currency={currentCurrency} />);
     } else if (menu == "expense") {
-      props.menuselected(<Expense setExpense={setExpense} />);
+      props.menuselected(
+        <Expense currency={currentCurrency} setExpense={setExpense} />
+      );
     } else if (menu == "setting") {
       props.menuselected(<Setting />);
     }
--- src/Components/Setting/Setting.jsx
+++ src/Components/Setting/Setting.jsx
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "../Card/Card";
+
 export const Setting = () => {
+  const [formData, setFormData] = useState();
+  const handleSettingsChange = (e) => {
+    setFormData({
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSave = () => {
+    document.cookie = "expense_currency=;expires=Thu, 01 Jan 1970 00:00:00 UTC";
+    const date = new Date();
+    date.setFullYear(date.getFullYear() + 4); // Add years to current date
+    const expires = "expires=" + date.toUTCString();
+    document.cookie =
+      "expense_currency=" + JSON.stringify(formData) + ";" + expires;
+  };
   return (
     <div>
       <h2 className="text-3xl font-mono text-center">Settings</h2>
       <Card>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded float-right">
+        <button
+          onClick={handleSave}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded float-right"
+        >
           Save
         </button>
-        <div class="relative w-64">
-          <label for="currency" class="block text-sm font-medium text-gray-700">
+        <div className="relative w-64">
+          <label
+            htmlFor="currency"
+            className="block text-sm font-medium text-gray-700"
+          >
             Select Currency
           </label>
           <select
             id="currency"
+            onChange={handleSettingsChange}
             name="currency"
-            class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md"
+            className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md"
           >
             <option value="usd">USD - US Dollar</option>
             <option value="eur">EUR - Euro</option>
