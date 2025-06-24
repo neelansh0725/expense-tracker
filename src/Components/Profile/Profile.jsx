eb239f98f1f9dfa655298709900ff370a7874e87
neelansh0725 <singhneelansh07@gmail.com>
2025-06-24 17:31:56 +0530

    Update Profile.jsx

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -8,8 +8,8 @@ export const Profile = () => {
           <img src="/profileimage.png" alt="" />
         </div>
         <div className="w-3/4 flex flex-col justify-center">
-          <h3 className="text-xl">Ishant</h3>
-          <p>Web Developer/ Freelancer</p>
+          <h3 className="text-xl">Neelansh</h3>
+          <p>Web Developer</p>
         </div>
       </div>
     </div>
